b975812c262e103ee58c048a8399d9d8af72badb
Darshan <sdarshu1510@gmail.com>
2022-03-19 10:20:24 +0530

    My First Commit..

---
+++ src/test/java/OrgTests/orgTest.java
@@ -0,0 +1,42 @@
+package OrgTests;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+	
+@Test
+	
+	public void orgtest_1() {
+		
+		System.out.println("org_1 is created");
+		
+	}
+	
+	@Test
+	
+public void orgttest_2() {
+		
+		System.out.println("org_2 is created");
+		
+	}
+	
+	@Test
+
+public void orgttest_3() {
+		
+		System.out.println("org_3 is created");
+		
+	}
+	
+	@Test
+	
+public void orgttest_4() {
+		
+		System.out.println("org_4SSSss is created");
+		
+		
+	}
+
+
+}
